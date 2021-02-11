121a71edf75542625df028101b9301eec2f90997
Mithun <mithain3@icloud.com>
2021-02-11 09:34:52 +0400

    CORS policy: include headers

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express()
 
+// CORS policy - allow headers
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'PUT');
+    next();
+})
+
 // initial setup
 app.use(express.json())
 app.use(bodyParser.urlencoded({
