4f432d4ad381f145c3572b04fcfe8be589efbd15
Mithun <mithain3@icloud.com>
2021-02-11 09:33:38 +0400

    Middlewares: logger & 404

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+// logger middleware
+app.use((req, res, next) => {
+    console.log("LOGGER: " + req.url)
+    next()
+})
+
 // connect to cloud MongoDB
 const MongoClient = require('mongodb').MongoClient;
 let db;
@@ -33,6 +39,11 @@ app.get('/collection/:collectionName', (req, res) => {
 
 // deploy /public
 app.use(express.static(path.resolve(__dirname, 'public')));
+
+// 404 middleware
+// app.use((req, res) => {
+//     res.status(404).send("Page not found!")
+// });
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running')
